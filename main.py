5650cde6e425ee042324894de3d5f60633a01b28
pedritoeldulce <franceskolyperez@gmail.com>
2022-11-17 22:14:47 -0500

    add operation delete_course and add Manejo de errores

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 
-from fastapi import FastAPI, status, Response
+from fastapi import FastAPI, status, Response, HTTPException
 #import uvicorn
 from typing import Optional, List
 from data import courses
+
+from fastapi.responses import PlainTextResponse, JSONResponse
+
 
 # importamos la clase Course (modelo)
 from models.Course import Course
@@ -11,7 +14,11 @@ app = FastAPI()
 
 @app.get('/courses')
 def get_courses():
+
     return {"courses":courses}
+    #return UJSONResponse()
+    #return JSONResponse(content={"Courses":courses,"message":"List Course", "status":"OK"})
+    #return JSONResponse(status_code=200, content={"courses":courses[0], "status":"OK"})
 
 @app.get('/courses/{id}')
 def get_course(id:int):
@@ -32,17 +39,34 @@ def get_content(id:int):
 
     print(courseFound)
     if courseFound:
-        # return {"content":courseFound[0]['tag'][0]['name'], "message":"course found"}
+        
         return {"content":courseFound[0].content, "message":"course Found"}
+        
 
     return {"message":"course not found"}
 
 
 @app.post('/courses')
 def create_course(course: Course):
-    courses.append(course)
-    return {"courses": courses, "message":"course added"}
 
+    # id nuevo a ingresar
+    n_id=course.id
+
+    # compara con las lista de ID de la lista courses
+    courseFound = list(filter(lambda course: course.id == n_id, courses))
+
+    if courseFound is not None:
+        raise HTTPException(status_code=400, detail="Duplicate id")
+
+    if len(course.name) == 0:
+        raise HTTPException(status_code=400, detail="field name required")
+
+    
+    
+    courses.append(course)    
+    #return {"courses": courses, "message":"course added"}
+    #return PlainTextResponse(status_code=201, content="course registed")
+    return JSONResponse(status_code=201, content={"message":"Course Registed", "status":"OK"})
 
 @app.put('/courses/{id}')
 def update_course(data: Course, id: int):
@@ -63,9 +87,13 @@ def update_course(data: Course, id: int):
 
 @app.delete('/courses/{id}')
 def delete_course(id:int):
-    userfound = list(filter(lambda course: course[0]["id"] == id, courses))
-    print(userfound)
+
+    userfound = list(filter(lambda course: course.id == id, courses))
+    
     if userfound:
+        # Eliminimanos el valor de la lista, solo contiene 1 valor a lista
+        courses.remove(userfound[0])
+
         return {"courses": courses, "message":"course deleted"}
 
     return {"message":"course not found"}
